432d8aad390dd4111c5aed927bb81a2292b7807f
Sagrika Bhandari <sagrikabhandari1997@gmail.com>
2024-03-07 17:04:01 +0000

    Your commit message here

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 import streamlit as st
 import preprocessor, helper
+import matplotlib.pyplot as plt
 
-# Sidebar using streamlit
+
+
 st.sidebar.title("Whatsapp Chat Analyser")
 
-# Variable that read the files
+
 uploaded_file = st.sidebar.file_uploader("Choose a file")
 if uploaded_file is not None:
     bytes_data = uploaded_file.getvalue()
@@ -23,6 +25,7 @@ if uploaded_file is not None:
     selected_user = st.sidebar.selectbox("Show analysis wrt",user_list)
 
     if st.sidebar.button("Show analysis"):
+
         num_messages, words, num_media_messages, num_links = helper.fetch_stats(selected_user,df)
 
         col1, col2, col3, col4 = st.columns(4)
@@ -39,5 +42,56 @@ if uploaded_file is not None:
         with col4:
             st.header("Links shared")
             st.title(num_links)
+
+        #finding the busiest user in the group
+        if selected_user == "Overall":
+            st.title('Most busy users')
+            x,new_df = helper.most_busy_users(df)
+            fig,ax = plt.subplots()
+
+            col1, col2 = st.columns(2)
+
+            with col1:
+                ax.bar(x.index, x.values, color= 'red')
+                plt.xticks(rotation='vertical')
+                st.pyplot(fig)
+
+            with col2:
+                st.dataframe(new_df)
+
+            # WordCloud
+            st.title('Wordcloud')
+            df_wc = helper.create_wordcloud(selected_user,df)
+            fig,ax = plt.subplots()
+            ax.imshow(df_wc)
+            st.pyplot(fig)
+
+            #most common words
+            most_common_df = helper.most_common_words(selected_user,df)
+            fig,ax = plt.subplots()
+            ax.bar(most_common_df[0], most_common_df[1])
+            plt.xticks(rotation='vertical')
+            st.title('Most common words')
+            st.pyplot(fig)
+
+
+
+            #emoji analysis
+            emoji_df = helper.emoji_helper(selected_user,df)
+            st.title('Most common emojis')
+
+            col1, col2 = st.columns(2)
+
+            with col1:
+                st.dataframe(emoji_df)
+            with col2:
+                fig,ax = plt.subplots()
+                ax.pie(emoji_df[1].head(),labels=emoji_df[0].head(),autopct='%0.2f')
+                st.pyplot(fig)
+
+
+
+
+
 
 
--- helper.py
+++ helper.py
@@ -1,4 +1,10 @@
 from urlextract import URLExtract
+from wordcloud import WordCloud
+import pandas as pd
+from collections import Counter
+import re
+
+import emoji
 extract = URLExtract()
 
 def fetch_stats(selected_user,df):
@@ -19,6 +25,99 @@ def fetch_stats(selected_user,df):
     for message in df['message']:
         links.extend(extract.find_urls(message))
     return num_messages, len(words), num_media_messages,len(links)
+
+def most_busy_users(df):
+    x = df['user'].value_counts().head()
+    df = round((df['user'].value_counts() / df.shape[0]) * 100, 2).reset_index().rename(
+        columns={'index': 'name', 'user': 'percent'})
+    return x, df
+
+def create_wordcloud(selected_user,df):
+    with open('stop_hinglish.txt', 'r') as f:
+        stop_words = f.read().splitlines()
+
+
+    if selected_user != 'Overall':
+        df = df[df['user'] == selected_user]
+
+
+    temp = df[df['user'] != 'group_notification']
+    temp = temp[temp['message'] != 'media omitted\n']
+
+    def remove_stop_words(message):
+        y = []
+        for word in message.lower().split():
+            if word not in stop_words:
+                y.append(word)
+        return ' '.join(y)
+
+
+
+    wc= WordCloud(width=500, height=500, min_font_size=10, background_color="white")
+    temp['message']= temp['message'].apply(remove_stop_words)
+    df_wc = wc.generate(temp['message'].str.cat(sep=" "))
+    return df_wc
+
+
+
+
+def most_common_words(selected_user, df):
+    with open('stop_hinglish.txt', 'r') as f:
+        stop_words = f.read().splitlines()
+
+    if selected_user != 'Overall':
+        df = df[df['user'] == selected_user]
+
+    temp = df[df['user'] != 'group_notification']
+    temp = temp[temp['message'] != 'media omitted\n']  # Update this line to filter out media omitted messages
+
+    words = []
+
+    for message in temp['message']:
+        for word in message.lower().split():
+            if word not in stop_words:
+                words.append(word)
+
+    most_common_df = pd.DataFrame(Counter(words).most_common(20))
+    return most_common_df
+
+
+def emoji_helper(selected_user, df):
+    if selected_user != 'Overall':
+        df = df[df['user'] == selected_user]
+
+    def extract_emojis(text):
+        emoji_pattern = re.compile("["
+                                   u"\U0001F600-\U0001F64F"  # emoticons
+                                   u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                                   u"\U0001F680-\U0001F6FF"  # transport & map symbols
+                                   u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                                   u"\U00002500-\U00002BEF"  # chinese char
+                                   u"\U00002702-\U000027B0"
+                                   u"\U00002702-\U000027B0"
+                                   u"\U000024C2-\U0001F251"
+                                   u"\U0001f926-\U0001f937"
+                                   u"\U00010000-\U0010ffff"
+                                   u"\u2640-\u2642"
+                                   u"\u2600-\u2B55"
+                                   u"\u200d"
+                                   u"\u23cf"
+                                   u"\u23e9"
+                                   u"\u231a"
+                                   u"\ufe0f"  # dingbats
+                                   u"\u3030"
+                                   "]+", flags=re.UNICODE)
+        return ''.join(emoji_pattern.findall(text))
+
+    # Example usage:
+    emojis = []
+    for message in df['message']:
+        emojis.extend(extract_emojis(message))
+
+    emoji_df = pd.DataFrame(Counter(emojis).most_common(len(Counter(emojis))))
+    return emoji_df
+
+
 
 
 
