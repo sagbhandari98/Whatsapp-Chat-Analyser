6ac4741452c0b7d5bb8375db7fa50e6a3f447353
Sagrika Bhandari <sagrikabhandari1997@gmail.com>
2024-03-11 15:52:16 +0000

    Complete code for Whatsapp chat analyser

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import preprocessor, helper
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 
 
@@ -13,7 +14,7 @@ if uploaded_file is not None:
     data = bytes_data.decode("utf-8")
     df = preprocessor.preprocess(data)
 
-    st.dataframe(df)
+
 
     # fetch unique users
 
@@ -27,8 +28,9 @@ if uploaded_file is not None:
     if st.sidebar.button("Show analysis"):
 
         num_messages, words, num_media_messages, num_links = helper.fetch_stats(selected_user,df)
-
+        st.title("Top Statistics")
         col1, col2, col3, col4 = st.columns(4)
+
 
         with col1:
             st.header("Total Messages")
@@ -42,6 +44,48 @@ if uploaded_file is not None:
         with col4:
             st.header("Links shared")
             st.title(num_links)
+
+        # monthly timeline
+        st.title("Monthly Timeline")
+        timeline = helper.monthly_timeline(selected_user, df)
+        fig, ax = plt.subplots()
+        ax.plot(timeline['time'], timeline['message'], color='green')
+        plt.xticks(rotation='vertical')
+        st.pyplot(fig)
+
+
+        #daily timeline
+        st.title("Daily Timeline")
+        daily_timeline = helper.daily_timeline(selected_user, df)
+        fig, ax = plt.subplots()
+        ax.plot(daily_timeline['only_date'], daily_timeline['message'], color='yellow')
+        plt.xticks(rotation='vertical')
+        st.pyplot(fig)
+
+        #Activity Map
+        st.title("Activity Map")
+        col1,col2 = st.columns(2)
+
+        with col1:
+            st.header("Most busy day")
+            busy_day = helper.week_activity_map(selected_user, df)
+            fig,ax = plt.subplots()
+            ax.bar(busy_day.index, busy_day.values)
+            st.pyplot(fig)
+        with col2:
+            st.header("Most busy month")
+            busy_month = helper.month_activity_map(selected_user,df)
+            fig, ax = plt.subplots()
+            ax.bar(busy_month.index, busy_month.values, color='orange')
+            plt.xticks(rotation='vertical')
+            st.pyplot(fig)
+        st.header("Weekly activity map")
+        user_heatmap = helper.activity_heatmap(selected_user, df)
+        fig,ax = plt.subplots()
+        ax = sns.heatmap(user_heatmap)
+        st.pyplot(fig)
+
+
 
         #finding the busiest user in the group
         if selected_user == "Overall":
--- preprocessor.py
+++ preprocessor.py
@@ -37,12 +37,25 @@ def preprocess(data):
     df['user'] = users
     df['message'] = messages
     df.drop(columns=['user_message'], inplace=True)
+    df['only_date'] = df['dates'].dt.date
 
     df['year'] = df['dates'].dt.year
+    df['month_num'] = df['dates'].dt.month
     df['month'] = df['dates'].dt.month_name()
     df['day'] = df['dates'].dt.day
+    df['day_name'] = df['dates'].dt.day_name()
     df['hour'] = df['dates'].dt.hour
     df['minute'] = df['dates'].dt.minute
+
+    period = []
+    for hour in df[['day_name', 'hour']]['hour']:
+        if hour == 23:
+            period.append(str(hour) + "-" + str('00'))
+        elif hour == 0:
+            period.append(str('00') + "-" + str(hour + 1))
+        else:
+            period.append(str(hour) + "-" + str(hour + 1))
+    df['period'] = period
 
     return df
 
